a3c9f94ef220dd2eaee0c8a564902b144f05a8f1
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2025-08-27 11:38:58 -0300

    fix metodo post

--- ocr/views.py
+++ ocr/views.py
@@ -12,31 +12,41 @@ import platform
 if platform.system() == 'Windows':
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
-# Configurar ruta de Tesseract
+
 
 class OCRAPIView(APIView):
     parser_classes = [MultiPartParser]
 
     def post(self, request, format=None):
-        image_file = request.data.get('image')
-        if not image_file:
+        image_files = request.FILES.getlist('images')  # ahora espera un array de imágenes
+
+        if not image_files:
             return Response({'error': 'No se proporcionó ninguna imagen'}, status=400)
 
+        results = []
+
         try:
-            # Abrir y preprocesar la imagen
-            image = Image.open(image_file).convert('L')  # Escala de grises
-            image = image.filter(ImageFilter.MedianFilter())
-            enhancer = ImageEnhance.Contrast(image)
-            image = enhancer.enhance(2)
+            for image_file in image_files:
+                # Preprocesamiento
+                image = Image.open(image_file).convert('L')
+                image = image.filter(ImageFilter.MedianFilter())
+                enhancer = ImageEnhance.Contrast(image)
+                image = enhancer.enhance(2)
 
-            # Convertir PIL a array OpenCV
-            image_np = np.array(image)
-            _, thresh = cv2.threshold(image_np, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                # OpenCV
+                image_np = np.array(image)
+                _, thresh = cv2.threshold(image_np, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-            # Aplicar OCR con idioma español
-            custom_config = r'--oem 3 --psm 6'
-            text = pytesseract.image_to_string(thresh, lang='spa', config=custom_config)
+                # OCR
+                custom_config = r'--oem 3 --psm 6'
+                text = pytesseract.image_to_string(thresh, lang='spa', config=custom_config)
 
-            return Response({'text': text})
+                results.append({
+                    "filename": image_file.name,
+                    "text": text.strip()
+                })
+
+            return Response({'results': results})
+
         except Exception as e:
             return Response({'error': str(e)}, status=500)
