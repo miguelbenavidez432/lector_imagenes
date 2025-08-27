1862a298d3f0795f0969ef9c49c6ba1b0366215c
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2025-08-27 00:26:38 -0300

    fix metodo post

--- ocr/views.py
+++ ocr/views.py
@@ -6,6 +6,8 @@ from PIL import Image, ImageFilter, ImageEnhance
 import numpy as np
 import cv2
 import os
+import platform
+
 
 if platform.system() == 'Windows':
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
